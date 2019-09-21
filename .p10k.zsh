b026226d2aa6b9f8118ecc1a4636edb7dbc42417
Alex Markovits <amarkovits@oneconcern.com>
2019-09-20 17:29:48 -0700

    better background
    Signed-off-by: Alex Markovits <amarkovits@oneconcern.com>

--- .p10k.zsh
+++ .p10k.zsh
@@ -171,7 +171,7 @@
   fi
 
   # Default background color.
-  typeset -g POWERLEVEL9K_BACKGROUND=236
+  typeset -g POWERLEVEL9K_BACKGROUND=238
 
   # Separator between same-color segments on the left.
   typeset -g POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR='%242F\uE0B1'
